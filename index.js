6b4805da667dff1d59a76a15037da88d09d86778
Davy Engone <davy@hackages.io>
2021-08-09 11:09:28 +0200

    starting point

---
+++ index.js
@@ -0,0 +1,13 @@
+// const responsePromise = fetch("https://api.github.com/users/davyengone");
+
+// responsePromise
+//   .then(function (response) {
+//     const dataPromise = response.json();
+//     return dataPromise;
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
